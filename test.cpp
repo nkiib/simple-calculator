94ea7e0c0c0d78a9aba7b44f103d5bee4be5040a
NKIIB nishikiout <48525357+nkiib@users.noreply.github.com>
2022-12-11 00:35:27 +0900

    test

--- test.cpp
+++ test.cpp
@@ -6,5 +6,14 @@ using namespace std;
 
 
 int main(){
-    vector<vector<int> > a;
+    string a = "rq";
+    double b;
+    try{
+        b = stod(a);
+    }
+    catch(std::invalid_argument){
+        cout << "Error" << endl;
+        return 0;
+    }
+    cout << b << endl;
 }
